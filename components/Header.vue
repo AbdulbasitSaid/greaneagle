5db2a1f25052bd2ea7d60a389aff9ea40796ed37
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-08 08:02:05 +0100

    header update

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <div class="h-24 bg-primary"></div>
+    <div class="h-48 w-screen">
+      <div class="w-screen bg-bg_pop_up">
+        <div
+          class="h-12 flex space-x-4 items-center max-w-9xl content-center justify-around"
+        >
+          <div class="w-20 h-3 bg-red-300"></div>
+          <div class="w-20 h-3 bg-red-300"></div>
+        </div>
+      </div>
+      <div class="h-20"></div>
+      <div class="h-14 bg-primary"></div>
+    </div>
   </div>
 </template>
 

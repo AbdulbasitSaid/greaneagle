3b106dea394aa9a723bc4ed6ebf9fc623f4edaa8
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-12 08:59:24 +0100

    home completed

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,8 @@
         <div class="flex-col">
           <product-list-title>Explore our categories</product-list-title>
           <products></products>
+          <product-list-title>Best deals</product-list-title>
+          <products></products>
         </div>
       </div>
       <subscribe-now></subscribe-now>

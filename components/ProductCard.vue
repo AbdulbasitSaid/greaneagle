06beed216a11e403d6ca0c8bab4860bfecda80fa
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-12 20:32:31 +0100

    detail in progress

---
+++ components/ProductCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex-col w-48 space-y-2">
+      <div class="">
+        <nuxt-link to="product_detail">
+          <slot></slot>
+        </nuxt-link>
+      </div>
+      <div class="text-sm font-thin">V07106</div>
+      <div class="text-sm font-thin">
+        General V07106 Electric Circuit Breaker
+      </div>
+      <div>$105.00</div>
+      <button class="border px-4 py-2 text-sm">Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- components/Products.vue
+++ components/Products.vue
@@ -13,202 +13,60 @@
       @selected="setSelected"
     >
       <product-tab :isSelected="selected === 'Mechanical Parts'">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product2.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product3.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product4.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product5.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product6.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product1.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product3.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product4.png" alt="" />
+        </product-card>
       </product-tab>
       <product-tab :isSelected="selected === 'Electrical Parts'">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product3.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product6.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product4.png" alt="" />
+        </product-card>
       </product-tab>
       <product-tab :isSelected="selected === 'Electronics'">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product6.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product3.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product3.png" alt="" />
+        </product-card>
       </product-tab>
+
       <product-tab :isSelected="selected === 'Power supplies'">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product2.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product2.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product5.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product6.png" alt="" />
+        </product-card>
       </product-tab>
       <product-tab :isSelected="selected === 'Systems & solutions'">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product4.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product3.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product3.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product2.png" alt="" />
+        </product-card>
+        <product-card>
+          <img class="h-56" src="~/assets/images/product1.png" alt="" />
+        </product-card>
       </product-tab>
     </product-tab-nav>
   </div>
 </template>
 
 <script>
+import ProductCard from "./ProductCard.vue";
 export default {
+  components: { ProductCard },
   data() {
     return { selected: "Mechanical Parts" };
   },

37d125c2e96a9abdbe61dbab5d979fc7ce38f0f4
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-13 04:40:48 +0100

    updates

--- pages/product_detail.vue
+++ pages/product_detail.vue
@@ -1,5 +1,166 @@
 <template>
-  <div><div></div></div>
+  <div class="py-10 px-20">
+    <div class="flex justify-between">
+      <nuxt-link to="store" class="flex items-center gap-2">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="10"
+            cy="10"
+            r="9.5"
+            fill="#1FBE5D"
+            fill-opacity="0.1"
+            stroke="#1FBE5D"
+          />
+          <path
+            opacity="0.3"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M14.25 11.125C14.5952 11.125 14.875 10.8452 14.875 10.5C14.875 10.1548 14.5952 9.875 14.25 9.875H6.75C6.40482 9.875 6.125 10.1548 6.125 10.5C6.125 10.8452 6.40482 11.125 6.75 11.125H14.25Z"
+            fill="#2AA580"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M10.9419 13.8081C11.186 14.0521 11.186 14.4479 10.9419 14.6919C10.6979 14.936 10.3021 14.936 10.0581 14.6919L6.30806 10.9419C6.07145 10.7053 6.06317 10.3243 6.28928 10.0777L9.72678 6.32767C9.96002 6.07322 10.3554 6.05603 10.6098 6.28928C10.8643 6.52252 10.8815 6.91788 10.6482 7.17233L7.61509 10.4812L10.9419 13.8081Z"
+            fill="#2AA580"
+          />
+        </svg>
+        Back to store
+      </nuxt-link>
+      <div>
+        <svg
+          width="64"
+          height="24"
+          viewBox="0 0 64 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect opacity="0.1" width="24" height="24" rx="3" fill="#455065" />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12 6.8175C12.8175 5.8575 14.07 5.25 15.375 5.25C17.685 5.25 19.5 7.065 19.5 9.375C19.5 12.2077 16.9542 14.5162 13.097 18.0139L13.0875 18.0225L12 19.0125L10.9125 18.03L10.8829 18.0031C7.03697 14.5082 4.5 12.2027 4.5 9.375C4.5 7.065 6.315 5.25 8.625 5.25C9.93 5.25 11.1825 5.8575 12 6.8175ZM12 16.9875L12.075 16.9125C15.645 13.68 18 11.5425 18 9.375C18 7.875 16.875 6.75 15.375 6.75C14.22 6.75 13.095 7.4925 12.705 8.52H11.3025C10.905 7.4925 9.78 6.75 8.625 6.75C7.125 6.75 6 7.875 6 9.375C6 11.5425 8.355 13.68 11.925 16.9125L12 16.9875Z"
+            fill="#455065"
+          />
+          <rect
+            opacity="0.1"
+            x="40"
+            width="24"
+            height="24"
+            rx="3"
+            fill="#455065"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M52 9.75V7.5C49.5175 7.5 47.5 9.5175 47.5 12C47.5 12.7575 47.6875 13.4775 48.025 14.1L46.93 15.195C46.345 14.2725 46 13.1775 46 12C46 8.685 48.685 6 52 6V3.75L55 6.75L52 9.75ZM55.975 9.9L57.07 8.805C57.655 9.7275 58 10.8225 58 12C58 15.315 55.315 18 52 18V20.25L49 17.25L52 14.25V16.5C54.4825 16.5 56.5 14.4825 56.5 12C56.5 11.2425 56.305 10.53 55.975 9.9Z"
+            fill="#455065"
+          />
+        </svg>
+      </div>
+    </div>
+    <div class="w-full flex items-start justify-center min-h-96 py-10 px-20">
+      <div class="w-3/5 flex-none">
+        <div class="flex flex-col gap-6">
+          <div class="border w-16 h-24 border-primary"></div>
+          <div class="border w-16 h-24 border-gray-100"></div>
+          <div class="border w-16 h-24 border-gray-50"></div>
+        </div>
+      </div>
+      <!-- text -->
+      <div class="flex flex-col justify-start gap-4">
+        <div class="flex items-center gap-2 text-sm">
+          <svg
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle opacity="0.2" cx="7" cy="7" r="7" fill="#1FBE5D" />
+            <circle cx="7" cy="7" r="4" fill="#1FBE5D" />
+          </svg>
+          12 quantity In Stock
+        </div>
+        <div class="text-xl">General Electric V07106 Circuit Breaker ...</div>
+        <div class="py-2 flex items-center gap-4">
+          <svg
+            width="107"
+            height="15"
+            viewBox="0 0 107 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M76.5 10.7937L80.3625 13.125L79.3375 8.73125L82.75 5.775L78.2563 5.39375L76.5 1.25L74.7438 5.39375L70.25 5.775L73.6625 8.73125L72.6375 13.125L76.5 10.7937Z"
+              fill="#F3B743"
+            />
+            <path
+              d="M53.5 10.7937L57.3625 13.125L56.3375 8.73125L59.75 5.775L55.2563 5.39375L53.5 1.25L51.7438 5.39375L47.25 5.775L50.6625 8.73125L49.6375 13.125L53.5 10.7937Z"
+              fill="#F3B743"
+            />
+            <path
+              d="M30.5 10.7937L34.3625 13.125L33.3375 8.73125L36.75 5.775L32.2563 5.39375L30.5 1.25L28.7438 5.39375L24.25 5.775L27.6625 8.73125L26.6375 13.125L30.5 10.7937Z"
+              fill="#F3B743"
+            />
+            <path
+              d="M7.5 10.7937L11.3625 13.125L10.3375 8.73125L13.75 5.775L9.25625 5.39375L7.5 1.25L5.74375 5.39375L1.25 5.775L4.6625 8.73125L3.6375 13.125L7.5 10.7937Z"
+              fill="#F3B743"
+            />
+            <path
+              d="M99.5 10.7937L103.363 13.125L102.337 8.73125L105.75 5.775L101.256 5.39375L99.5 1.25L97.7438 5.39375L93.25 5.775L96.6625 8.73125L95.6375 13.125L99.5 10.7937Z"
+              fill="#F2F2F2"
+            />
+          </svg>
+          4 Reviews
+          <nuxt-link to="/" class="underline text-primary"
+            >Write a review</nuxt-link
+          >
+        </div>
+        <div class="text-gray-500">Description</div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          molestie, nulla id scelerisque luctus, odio nisi consequat erat, eu
+          vestibulum velit massa vel felis.
+        </div>
+        <div>
+          <p class="text-gray-500">Brand:</p>
+          General electric
+        </div>
+        <div>
+          <p class="text-gray-500">Product code:</p>
+          V07106
+        </div>
+        <div class="flex items-center gap-10">
+          <!-- increase -->
+          <div class="flex space-x-2">
+            <input
+              type="button"
+              value="-"
+              class="border p-4 bg-white rounded"
+            />
+            <div class="border p-4 bg-white rounded">1</div>
+
+            <input
+              type="button"
+              value="+"
+              class="border p-4 bg-white rounded"
+            />
+          </div>
+          <!-- end  -->
+          <div class="rounded bg-primary py-4 px-6 text-white">
+            Request quote
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- pages/store.vue
+++ pages/store.vue
@@ -94,93 +94,34 @@
       </div>
       <!-- end top -->
       <div class="p-4 grid grid-cols-4 gap-6">
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product2.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product3.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product4.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product5.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
-        <div class="flex-col w-48 space-y-2">
-          <div class="">
-            <img class="h-56" src="~/assets/images/product1.png" alt="" />
-          </div>
-          <div class="text-sm font-thin">V07106</div>
-          <div class="text-sm font-thin">
-            General V07106 Electric Circuit Breaker
-          </div>
-          <div>$105.00</div>
-          <button class="border px-4 py-2 text-sm">Add to Cart</button>
-        </div>
+        <product-card>
+          <img src="~assets/images/product1.png" />
+        </product-card>
+        <product-card>
+          <img src="~assets/images/product2.png" />
+        </product-card>
+        <product-card>
+          <img src="~assets/images/product3.png" />
+        </product-card>
+        <product-card>
+          <img src="~assets/images/product4.png" />
+        </product-card>
+        <product-card>
+          <img src="~assets/images/product5.png" />
+        </product-card>
+        <product-card>
+          <img src="~assets/images/product6.png" />
+        </product-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductCard from "../components/ProductCard.vue";
 import Products from "../components/Products.vue";
 export default {
-  components: { Products },
+  components: { Products, ProductCard },
 };
 </script>
 
